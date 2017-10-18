041b1ca6f1c4985e952a71ceb2d1b3508527353a
Jérémie Zanone <jeremie.zanone@heig-vd.ch>
2017-10-18 15:43:04 +0200

    agent

--- src/agent.js
+++ src/agent.js
@@ -1,28 +1,89 @@
 const request = require('superagent');
+const credentialsGitHub = require('../github-credentials.json');
+const Storage = require('./storage.js');
 
 class Agent {
   constructor(credentials) {
     this.credentials = credentials;
   }
 
-  fetchAndProcessAllRepos(owner, allRepoSAreAvailable) {
-    const targerUrl = `https://api.github.com/users/${owner}/repos`;
+  fetchAndProcessAllCommits(owner, repo, allCommitsAreAvailable) {
+    let commits = [];
+
+    function fetchAndProcessPage(urlCommits, credentials) {
+      request
+        .get(urlCommits)
+        .auth(credentials.username, credentials.token)
+        .end((err3, res3) => {
+          commits = commits.concat(res3.body);
+          if (res3.links.next) {
+            fetchAndProcessPage.bind(this)(res3.links.next, credentials);
+          } else {
+            allCommitsAreAvailable(null, commits);
+          }
+        });
+    }
+    const targetUrlCommits = `https://api.github.com/repos/${owner}/${repo}/commits`;
+    fetchAndProcessPage.bind(this)(targetUrlCommits, this.credentials);
+  }
+
+  fetchAndProcessAllRepos(owner, allReposAreAvailable) {
+    const targetUrl = `https://api.github.com/users/${owner}/repos`;
     let repos = [];
+
     function fetchAndProcessPage(pageUrl, credentials) {
       request
         .get(pageUrl)
         .auth(credentials.username, credentials.token)
         .end((err, res) => {
-          repos = repos.concat(res.body);
+          const reposInPage = res.body;
+          repos = repos.concat(reposInPage);
+
+          let numberOfReposForWhichWeNeedToGetCommits = repos.length;
+          reposInPage.forEach((repo) => {
+            const repoName = repo.name;
+
+            this.fetchAndProcessAllCommits(owner, repoName, (err2, commits) => {
+              const repoToAugment = repo;
+              repoToAugment.commits = commits;
+              numberOfReposForWhichWeNeedToGetCommits -= 1;
+              if (numberOfReposForWhichWeNeedToGetCommits === 0) {
+                allReposAreAvailable(null, repos);
+              }
+            });
+          });
+
           if (res.links.next) {
-            fetchAndProcessPage(res.links.next, credentials);
-          } else {
-            allRepoSAreAvailable(null, repos);
+            fetchAndProcessPage.bind(this)(res.links.next, credentials);
           }
         });
     }
-    fetchAndProcessPage(targerUrl, this.credentials);
+    fetchAndProcessPage.bind(this)(targetUrl, this.credentials);
   }
 }
 
-module.exports = Agent;
+const owner = 'youtube';
+const agent = new Agent(credentialsGitHub);
+const agentOwner = 'CoolPolishGuy';
+const agentRepo = 'TWEB-GitHub-Analytics-Agent';
+const publisher = new Storage(agentOwner, credentialsGitHub.token, agentRepo);
+
+agent.fetchAndProcessAllRepos(owner, (err, repos) => {
+  let beautifyJSON = [];
+  repos.forEach((repo) => {
+    const info = {};
+    info.name = repo.name;
+    info.commits = repo.commits.length;
+    beautifyJSON.push(info);
+  });
+  beautifyJSON = beautifyJSON.sort((a, b) => b.commits - a.commits);
+  console.log(beautifyJSON);
+  /* publisher.publish('data/data.json', JSON.stringify(beautifyJSON), 'new version available', (error, result) => {
+    if (result) {
+      console.log('Data pushed');
+    } else {
+      console.log(`Sth bad happens : ${error.body}`);
+    }
+  }); */
+  module.exports = Agent;
+});
